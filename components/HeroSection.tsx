5df8b864a6ca1da842ec9eb033423f9f4d03abde
Norman Cade <norman_cade32@yahoo.com>
2025-07-15 21:28:54 -0400

    Used flex-box to be responsive to smaller screens better

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -5,12 +5,12 @@ import Button from "./general/Button";
 
 const HeroSection = () => {
   return (
-    <div className="py-[60px] border-b-1 border-(--neutral-gray)">
-      <div className="w-fit mx-auto text-center px-4">
-        <h1 className="font-extrabold text-3xl p-8">
+    <div className="py-[60px] border-b-1 border-(--neutral-gray) flex flex-col items-center justify-center ">
+      <div className="w-fit px-4 flex flex-col  items-center justify-center ">
+        <h1 className="font-semibold text-3xl p-8 text-center">
           Master Software Engineering, One Question at a Time
         </h1>
-        <p className="w-170 mx-60">
+        <p className="w-[70%] text-center">
           Build your own study guides or practice with hand-picked coding questions covering
           algorithms, system design, front-end, and more.
         </p>
