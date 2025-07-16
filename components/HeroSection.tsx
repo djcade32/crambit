75c9c24f8c2f3193810069fefcea5d1ce60547ba
Norman Cade <norman_cade32@yahoo.com>
2025-07-15 21:16:22 -0400

    Use padding instead of height.

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -5,7 +5,7 @@ import Button from "./general/Button";
 
 const HeroSection = () => {
   return (
-    <div className="h-70 border-b-1 border-(--neutral-gray)">
+    <div className="py-[60px] border-b-1 border-(--neutral-gray)">
       <div className="w-fit mx-auto text-center px-4">
         <h1 className="font-extrabold text-3xl p-8">
           Master Software Engineering, One Question at a Time
