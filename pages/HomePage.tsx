75f6890ee318e6561553077eba9311380da78f2a
Kendrick Wilkerson <Kendrickwilkerson22@gmail.com>
2025-07-16 21:08:45 -0400

    Added incoming changes

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import QuestionSection from "@/sections/home/QuestionSection";
 import HeroSection from "@/sections/home/HeroSection";
+import Filter from "@/components/Filter";
 
 const HomePage = () => {
   return (
     <div className="page-container">
       <HeroSection />
+      <Filter />
       <QuestionSection />
     </div>
   );
