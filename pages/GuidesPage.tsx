f99ebaff49beea3d4193bb82aa42f3fb1253385b
Norman Cade <norman_cade32@yahoo.com>
2025-07-24 17:02:18 -0400

    Implemented Create Guide Page

--- pages/GuidesPage.tsx
+++ pages/GuidesPage.tsx
@@ -1,10 +1,29 @@
-import QuestionsTable from "@/components/QuestionsTable/QuestionsTable";
+"use client";
+
+import Button from "@/components/general/Button";
+import StudyGuide from "@/components/StudyGuide";
+import { DUMMY_STUDY_GUIDES } from "@/data/dummyData";
 import React from "react";
 
 const GuidesPage = () => {
   return (
-    <div className="flex flex-1 justify-center items-center">
-      <QuestionsTable />
+    <div className="page-container ">
+      <div className="py-9 flex flex-col items-center gap-5">
+        <h1 className="text-3xl text-center font-semibold">Create a Guide</h1>
+        <Button label="Create" onClick={() => console.log("clicked create")} />
+      </div>
+      <div className="grid grid-cols-3 gap-14 overflow-y-scroll w-full mx-auto p-7">
+        {DUMMY_STUDY_GUIDES.map((guide, index) => (
+          <StudyGuide
+            key={index}
+            id={guide.id}
+            title={guide.title}
+            questionsCount={guide.questionsCount}
+            lastUpdated={guide.lastUpdated}
+            createGuide
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- pages/Study/StudyPage.tsx
+++ pages/Study/StudyPage.tsx
@@ -4,11 +4,11 @@ import React from "react";
 
 const StudyPage = () => {
   return (
-    <div className="page-container px-7">
-      <div className="py-11">
+    <div className="page-container">
+      <div className="py-9">
         <h1 className="text-3xl text-center font-semibold">Choose a guide to start studying</h1>
       </div>
-      <div className="grid grid-cols-3 gap-14 overflow-y-scroll w-full mx-auto">
+      <div className="grid grid-cols-3 gap-14 overflow-y-scroll w-full mx-auto p-7">
         {DUMMY_STUDY_GUIDES.map((guide, index) => (
           <StudyGuide
             key={index}
