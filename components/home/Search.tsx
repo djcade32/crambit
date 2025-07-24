35c6e203917392dd801cc9968c95276b7caa618a
Norman Cade <norman_cade32@yahoo.com>
2025-07-24 15:57:32 -0400

    Fixed Search component css

--- components/home/Search.tsx
+++ components/home/Search.tsx
@@ -2,13 +2,31 @@ import * as React from "react";
 import { Search as SearchIcon } from "lucide-react";
 
 export default function Search() {
+  const [focused, setFocused] = React.useState(false);
   return (
-    <div className="flex items-center gap-2 w-[45%] border-2 border-(--neutral-gray) rounded-[5px]">
-      <SearchIcon className="text-(--black) dark:text-(--white) ml-2" />
+    <div
+      className={`
+        flex
+        w-[45%]
+        border-2 border-(--neutral-gray) rounded-[5px]
+        transition-colors
+        ${focused && "bg-(--neutral-gray)/60"}
+        items-center gap-2 hover:bg-(--neutral-gray)/60 duration-200
+      `}
+    >
+      <SearchIcon className="ml-2 text-(--black) dark:text-(--white)" />
       <input
         type="text"
         placeholder="Search Question"
-        className="text-lg text-(--black) dark:text-(--white) placeholder:text-(--dark-gray) selection:border-0 focus:outline-none focus:ring-0 caret-(--black) dark:caret-(--white) w-full p-2 hover:bg-(--neutral-gray)/60 transition-colors duration-200 rounded-[5px] focus:bg-(--neutral-gray)/60"
+        className="
+          w-full
+          p-2
+          text-lg text-(--black)
+          rounded-[5px]
+          dark:text-(--white) placeholder:text-(--dark-gray) selection:border-0 focus:outline-none focus:ring-0 caret-(--black) dark:caret-(--white) 
+        "
+        onFocus={() => setFocused(true)}
+        onBlur={() => setFocused(false)}
       />
     </div>
   );
